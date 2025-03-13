1035a77a72db9c8b8a064e429535bb9e51e1f488
nirajumaretiya <nirajumretiya7@gmail.com>
2025-03-13 17:37:44 +0530

    controller module

---
+++ src/controller.sv
@@ -0,0 +1,17 @@
+`default_nettype none
+`timescale 1ns/1ns
+
+module controller #(
+   parameter ADDR_BITS = 8,
+   parameter DATA_BITS = 16,
+   parameter NUM_CONSUMERS = 4,
+   parameter NUM_CHANNELS= 1,
+   parameter WRITE_ENABLE=1
+)(
+    input wire clk,
+    input wire reset,
+
+    input 
+);
+
+endmodule
